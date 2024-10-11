da82877c249213bf5bd57276242250510a7bd6bf
fullupe <fullupe@gmail.com>
2024-10-12 00:50:12 +0100

    metadata added

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import Footer from '@/components/Footer';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'LotteryTech Solutions',
+  title: 'Fortune360 Solutions',
   description: 'Providing cutting-edge lottery platform solutions',
 };
 
